5316564ac94dd96b4e48d516d2f802ed40e69e19
oscardavid10 <david_010@live.com.mx>
2022-03-29 11:12:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link href="css/bootstrap.min.css" rel="stylesheet"></link>
-    
+
     <script src="js/jquery-3.5.1.js"></script>
 
     <title>Document</title>
 </head>
 <body>
-    
+<form>
+  <div class="mb-3">
+    <label for="id" class="form-label">Ingresa tu ID</label>
+    <input type="text" class="form-control" id="id" aria-describedby="ID">
+    <div id="idHelp" class="form-text">Lo puedes encontrar en tu ticket como ID</div>
+  </div>
+  <div class="mb-3">
+    <label for="cr" class="form-label">Ingresa tu CR</label>
+    <input type="text" class="form-control" id="cr">
+  </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
 </body>
 </html>
