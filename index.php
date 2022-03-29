9824927f7481d272d851a3160b282c8ab4ce8dac
oscardavid10 <david_010@live.com.mx>
2022-03-29 11:49:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet"></link>
 
     <script src="js/jquery-3.5.1.js"></script>
 
@@ -29,9 +28,7 @@
 </form>
 
 <script>
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+
 </script>
 </body>
 </html>
