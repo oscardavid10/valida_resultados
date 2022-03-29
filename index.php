6fe420921e6678c64f15eec1c82ba7741da04d73
oscardavid10 <david_010@live.com.mx>
2022-03-29 12:07:19 -0600

    Add bootstrap js

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <link href="css/bootstrap.min.css" rel="stylesheet"></link>
 
     <script src="js/jquery-3.5.1.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/popper.min.js"></script>
+
     
 
     <title>Document</title>
