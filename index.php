c39518a2c5afe152cc0e00b044511b0f40511d1e
oscardavid10 <david_010@live.com.mx>
2022-03-29 12:09:21 -0600

    Add popper

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet"></link>
 
-    <script src="js/jquery-3.5.1.js"></script>
+    <script src="js/jquery-3.3.1.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/popper.min.js"></script>
 
